dd8330870b585c4657f6e728340bc80b7e9610d4
hebohan <hebohan@gmail.com>
2018-08-14 22:40:50 +0800

    Api项目修改

--- api/Program.cs
+++ api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace api
+namespace Api
 {
     public class Program
     {
