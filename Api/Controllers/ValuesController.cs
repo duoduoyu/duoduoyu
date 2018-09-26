6ec0a2e4f1306471a52d571318b2492534b114a4
linchang <linchang@exexm.com>
2018-09-26 10:48:58 +0800

    测试分支

--- Api/Controllers/ValuesController.cs
+++ Api/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace Api.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value" + id;
         }
 
         // POST api/values
